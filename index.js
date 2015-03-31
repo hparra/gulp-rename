194b219e2324709cf026c097f365dccc3f5aa7a7
Olivier Louvignes <olivier@mg-crea.com>
2015-03-31 20:23:52 +0100

    chore(global): use two-space indenting and single quotes (closes #45)

--- index.js
+++ index.js
@@ -1,62 +1,65 @@
-var Stream = require("stream"),
-	Path = require("path");
+'use strict';
+
+var Stream = require('stream');
+var Path = require('path');
 
 function gulpRename(obj) {
-	"use strict";
 
-	var stream = new Stream.Transform({objectMode: true});
+  var stream = new Stream.Transform({objectMode: true});
 
-	function parsePath(path) {
-		var extname = Path.extname(path);
-		return {
-			dirname: Path.dirname(path),
-			basename: Path.basename(path, extname),
-			extname: extname
-		};
-	}
+  function parsePath(path) {
+    var extname = Path.extname(path);
+    return {
+      dirname: Path.dirname(path),
+      basename: Path.basename(path, extname),
+      extname: extname
+    };
+  }
 
-	stream._transform = function(file, unused, callback) {
+  stream._transform = function(file, unused, callback) {
 
-		var parsedPath = parsePath(file.relative);
-		var path;
+    var parsedPath = parsePath(file.relative);
+    var path;
 
-		var type = typeof obj;
+    var type = typeof obj;
 
-		if (type === "string" && obj !== "") {
+    if (type === 'string' && obj !== '') {
 
-			path = obj;
+      path = obj;
 
-		} else if (type === "function") {
+    } else if (type === 'function') {
 
-			obj(parsedPath);
-			path = Path.join(parsedPath.dirname, parsedPath.basename + parsedPath.extname);
+      obj(parsedPath);
+      path = Path.join(parsedPath.dirname, parsedPath.basename + parsedPath.extname);
 
-		} else if (type === "object" && obj !== undefined && obj !== null) {
+    } else if (type === 'object' && obj !== undefined && obj !== null) {
 
-			var dirname = "dirname" in obj ? obj.dirname : parsedPath.dirname,
-				prefix = obj.prefix || "",
-				suffix = obj.suffix || "",
-				basename = "basename" in obj ? obj.basename : parsedPath.basename,
-				extname = "extname" in obj ? obj.extname : parsedPath.extname;
+      var dirname = 'dirname' in obj ? obj.dirname : parsedPath.dirname,
+        prefix = obj.prefix || '',
+        suffix = obj.suffix || '',
+        basename = 'basename' in obj ? obj.basename : parsedPath.basename,
+        extname = 'extname' in obj ? obj.extname : parsedPath.extname;
 
-			path = Path.join(dirname, prefix + basename + suffix + extname);
+      path = Path.join(dirname, prefix + basename + suffix + extname);
 
-		} else {
-			callback(new Error("Unsupported renaming parameter type supplied"), undefined);
-			return;
-		}
+    } else {
 
-		file.path = Path.join(file.base, path);
+      callback(new Error('Unsupported renaming parameter type supplied'), undefined);
+      return;
 
-		// Rename sourcemap if present
-		if (file.sourceMap) {
-			file.sourceMap.file = file.relative;
-		}
+    }
 
-		callback(null, file);
-	};
+    file.path = Path.join(file.base, path);
 
-	return stream;
+    // Rename sourcemap if present
+    if (file.sourceMap) {
+      file.sourceMap.file = file.relative;
+    }
+
+    callback(null, file);
+  };
+
+  return stream;
 }
 
 module.exports = gulpRename;
